fba4d9e3eea31c20255f2894fd0456898f72c77c
Jan McSorley <mcsorleyjan@gmail.com>
2023-03-26 16:20:28 -0500

    Add revised variables for strings, numbers, and booleans and add 9 console.log entries

--- variables.js
+++ variables.js
@@ -16,8 +16,27 @@ studentName = "Jack Revelett"
 pronouns = "he/him"
 dateOfBirth ="March 25, 2005"
 favoriteFood = "Anything Cajun"
-// console.log (studentName) (pronouns) (dateOfBirth) (favoriteFood)
+console.log (studentName) (pronouns) (dateOfBirth) (favoriteFood)
 console.log (studentName)
 console.log (pronouns)
 console.log (dateOfBirth)
 console.log (favoriteFood)
+studentName = "Loretta Chang"
+pronouns = "they/them"
+dateOfBirth = "January 27, 2007"
+favoriteFood = "Tacos"
+schoolGrade = 10
+numPets = 0
+numSiblings = 3
+driverTestScore = 81
+englishSpeaker = false
+boyScout = true
+console.log (studentName)
+console.log (pronouns)
+console.log (dateOfBirth)
+console.log (schoolGrade)
+console.log (numPets)
+console.log (numSiblings)
+console.log (driverTestScore)
+console.log (englishSpeaker)
+console.log (boyScout)
