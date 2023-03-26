b819021210c846f0cc1e51ae37b76b99ef786279
Jan McSorley <mcsorleyjan@gmail.com>
2023-03-26 15:31:45 -0500

    Add 4 number variables

--- variables.js
+++ variables.js
@@ -2,3 +2,7 @@ var name = "Logan McSorley"
 var pronouns = "she/her"
 var dateOfBirth = "May 20, 2005"
 var favoriteFood = "Spaghetti"
+var schoolGrade = 12
+var numPets = 4
+var numSiblings = 2
+var driverTestScore = 93
